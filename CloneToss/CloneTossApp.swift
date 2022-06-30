42a2bd6ce4197d394c652e8a952b3c3d66f56545
SH0123 <sanghyoboy@naver.com>
2022-06-30 23:43:34 +0900

    Initial Commit

---
+++ CloneToss/CloneTossApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloneTossApp.swift
+//  CloneToss
+//
+//  Created by sanghyo on 2022/06/30.
+//
+
+import SwiftUI
+
+@main
+struct CloneTossApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
