1450d37b8106139a4fb0151b9132d038ca3fd3f2
SH0123 <sanghyoboy@naver.com>
2022-07-04 23:11:58 +0900

    Feat: add tabbar layout

--- CloneToss/ContentView.swift
+++ CloneToss/ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+        VStack {
+            Spacer()
+            tabBar
+        }
+        .toolbar {
+            ToolbarItemGroup(placement: .navigationBarTrailing) {
+                HStack(spacing: 20) {
+                Image(systemName: "message.fill")
+                Image(systemName: "bell.fill")
+                }
+            }
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        }
+    }
+    
+    //MARK: - viewBuilder
+    @ViewBuilder
+    private var tabBar: some View {
+        let insets = EdgeInsets(top: 10, leading:  20, bottom: 40, trailing: 20)
+        HStack {
+            ForEach(TabBarIcon.allCases, id: \.self) {
+                TabBarIconContainer(imageName: $0.imageName, iconName: $0.iconName)
+                    .frame(maxWidth: .infinity, alignment: .bottom)
+            }
+        }
+        .padding(insets)
+        .background(.white)
     }
 }
 
